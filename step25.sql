17671dbcdff112515ddf638dcce41f82557ca4cb
michael-delegge <michael.w.delegge@gmail.com>
2017-12-02 10:21:11 -0500

    challenge

---
+++ step25.sql
@@ -0,0 +1,15 @@
+/*
+
+challenge
+
+Drop all tables/views
+*/
+
+DROP TABLE IF EXISTS teams;
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS player_stats;
+DROP TABLE IF EXISTS games_played;
+DROP TABLE IF EXISTS score;
+DROP VIEW IF EXISTS highest;
+DROP VIEW IF EXISTS total;
+DROP VIEW IF EXISTS wins;
