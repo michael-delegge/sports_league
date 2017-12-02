629529497be277d372ec54ff891f166320670e39
michael-delegge <michael.w.delegge@gmail.com>
2017-12-02 10:44:54 -0500

    submit version

---
+++ step23.sql
@@ -0,0 +1,13 @@
+/*
+
+step 23
+
+Create a single SQL script to print the contents of 
+each of the tables in the project.
+*/
+
+SELECT * FROM teams;
+SELECT * FROM players;
+SELECT * FROM player_stats;
+SELECT * FROM games_played;
+SELECT * FROM score;
